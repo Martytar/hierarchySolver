bbd93ff34c7d93f6da632f73ea875715fd0eed5a
Bogdan M <martynov.bogdan.ru@gmail.com>
2025-04-01 17:02:05 +0300

    Primary project structure

---
+++ src/hierarchySolver/__main__.py
@@ -0,0 +1,7 @@
+def run_main():
+    print('program is running')
+
+
+if __name__ == '__main__':
+    run_main()
+
---
+++ src/hierarchySolver/optimisation_methods.py
@@ -0,0 +1,11 @@
+from numpy import random
+import numpy as np
+
+#Здесь разместим методы стохастической оптимизации
+
+def optimize_by_monte_carlo():
+    print('Monte Carlo Optimization')
+
+
+def optimize_by_annealing():
+    print('Annealing Optimisation')
