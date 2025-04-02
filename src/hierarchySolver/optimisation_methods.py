921a2caacdc275eca3a007d9154d657f6b7a9ce9
Bogdan M <martynov.bogdan.ru@gmail.com>
2025-04-02 23:32:38 +0300

    Refactored algorithm of getting universally distributed spots from simplex (part of monte-carlo optimisation method)

--- src/hierarchySolver/optimisation_methods.py
+++ src/hierarchySolver/optimisation_methods.py
@@ -3,9 +3,21 @@ import numpy as np
 
 #Здесь разместим методы стохастической оптимизации
 
+def _generate_spot_from_simplex(dim, max_val):
+    gamma = [np.random.rand() for k in range(dim)]
+    spot = np.zeros(dim)
+
+    p1 = 1
+    p2 = gamma[0] ** (1 / dim)
+    spot[0] = max_val * (p1 - p2)
+    for i in range(1, dim):
+        p1 *= gamma[i - 1] ** (1 / (dim - i + 1))
+        p2 *= gamma[i] ** (1 / (dim - i))
+        spot[i] = max_val * (p1 - p2)
+    return spot
+
 def optimize_by_monte_carlo():
     print('Monte Carlo Optimization')
-
 
 def optimize_by_annealing():
     print('Annealing Optimisation')
