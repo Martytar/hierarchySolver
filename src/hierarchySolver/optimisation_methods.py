1c793d94208ec87482b54f2da300d6895e583e4b
Bogdan M <martynov.bogdan.ru@gmail.com>
2025-04-11 22:01:57 +0300

    Added MonteCarlo optimisation. Need to test it properly

--- src/hierarchySolver/optimisation_methods.py
+++ src/hierarchySolver/optimisation_methods.py
@@ -3,6 +3,10 @@ import numpy as np
 
 #Здесь разместим методы стохастической оптимизации
 
+
+#внутренний метод для корректной работы оптимизации монте-карло на симплексе.
+#Согласно размерности(dim) и максимальному значению параметров (max_val, скаляр)
+#выбирает на равностороннем симлексе точку из равномерного распределения
 def _generate_spot_from_simplex(dim, max_val):
     gamma = [np.random.rand() for k in range(dim)]
     spot = np.zeros(dim)
@@ -16,8 +20,26 @@ def _generate_spot_from_simplex(dim, max_val):
         spot[i] = max_val * (p1 - p2)
     return spot
 
-def optimize_by_monte_carlo():
-    print('Monte Carlo Optimization')
+def optimize_by_monte_carlo(f, specifications, spots_number):
+
+    if spots_number <= 0:
+        raise ValueError("Number of spots must be positive")
+
+    max_spot = np.zeros(f.input_dimension)
+    max_value = f.calculate(max_spot)
+    for i in range(spots_number):
+        spot = np.zeros(f.input_dimension)
+        for spec in specifications:
+            simplex_vals = _generate_spot_from_simplex(len(spec)-1, spec[-1])
+            for j in range(len(spec)-1):
+                spot[spec[j]] = simplex_vals[j]
+        value = f.calculate(spot)
+        if value > max_value:
+            max_spot = spot
+            max_value = value
+
+    return max_spot
+
 
 def optimize_by_annealing():
     print('Annealing Optimisation')
